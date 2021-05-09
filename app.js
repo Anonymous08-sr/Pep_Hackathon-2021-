de2caecc789247cc526c34de5fd2526d28c963cf
Shreyash <shreyash123.sr@gmail.com>
2021-05-09 13:50:20 +0530

    first_commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+async function getPriceFeed() {
+  try {
+    const siteUrl = "https://coinmarketcap.com/";
+    const { data } = await axios({
+      method: "GET",
+      url: siteUrl,
+    });
+
+    const $ = cheerio.load(data);
+    const elemSelector =
+      "#__next > div > div.main-content > div.sc-57oli2-0.dEqHl.cmc-body-wrapper > div > div > div.tableWrapper___3utdq.cmc-table-homepage-wrapper___22rL4 > table > tbody > tr";
+
+    const keys = [
+      "Rank",
+      "Name",
+      "Price",
+      "24h-Change",
+      "7d-Change",
+      "MarketCap",
+      "Volume",
+      "Circulating Supply",
+    ];
+
+    const coinArr = [];
+
+    $(elemSelector).each((parentIdx, parentElem) => {
+      let keyIdx = 0;
+      const coinObj = {};
+
+      if (parentIdx <= 9) {
+        $(parentElem)
+          .children()
+          .each((childIdx, childElem) => {
+            let tdValue = $(childElem).text();
+
+            if (keyIdx === 1 || keyIdx === 6) {
+              tdValue = $("p:first-child", $(childElem).html()).text();
+            }
+
+            if (tdValue) {
+              coinObj[keys[keyIdx]] = tdValue;
+              keyIdx++;
+            }
+          });
+        coinArr.push(coinObj);
+      }
+    });
+
+    return coinArr;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const app = express();
+
+app.get("/api/price-feed", async (req, res) => {
+  try {
+    const priceFeed = await getPriceFeed();
+    return res.status(200).json({
+      result: priceFeed,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
